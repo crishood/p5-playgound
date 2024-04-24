e339875a5397079c49361fcc1dba40831ec0aea3
Sir CrisHood <crishood13@gmail.com>
2024-04-23 21:08:51 -0500

    feat: install p5svg

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import './style.scss';
 
 import p5 from 'p5';
+import init, { p5SVG } from 'p5.js-svg';
+
+init(p5);
 
 // Define tu sketch p5
-const sketch = (p: p5) => {
+const sketch = (p: p5SVG) => {
   p.setup = () => {
-    p.createCanvas(400, 400);
+    p.createCanvas(400, 400, p.SVG);
     p.background(220);
   };
 
